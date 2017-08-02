035b5c71220a1a4e5da01d9314e2c33aeb729637
Tatianna <Tatianna@tetiannas-MBP.fios-router.home>
2017-08-01 23:04:28 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {	
-var timer;
 
 	var quiz = [
 			{ question: "What is Simba's Father's name?",
@@ -21,37 +20,43 @@ var timer;
 			{ question: "Who sailed across the sea to restore the heart of Ti Fiti?",
 			  answer: "Moana",
 			  options: ["Hercules","Tiana","Obama","Moana"]
-			},
-			
+			},	
 	]
 
+	var intervalId;
+	var timerNum = 30;
+	var correct = 0;
+	var incorrect = 0;
+	var unanswered = 0;
+	var selectedAnswers = [];
+	
 	var track = {
-		correct: 0,
-		incorrect: 0,
-		unanswered: 0,
-		timerNum: 30,
 
-		count: function() {
-			track.timerNum--;
+	 	run: function() {
+			intervalId = setInterval(track.decrement, 1000);
+	 	},
+	 	decrement: function() {
+	 		timerNum--;
+			$("#timer").html("<h2>" + timerNum + "<h2>");
 
-			$('#countdown').html(track.timerNum);
-			//won't stop
-			if (track.timerNum <= 0) {
-				console.log("Time's UP.");
-				track.done();
+			if (timerNum === 0) {
+				track.result();
+				alert("Time's Up!!")
+
 			}
-		},
+	 	},
 
 		startTrivia: function() {
 			//once we click on start we remove the start button and show the questions		
-			timer = setInterval(track.count,1000);
-			$('#timer').prepend('<h2>Time Remaining: <span id="countdown">30</span> Seconds</h2>');
 			$("#start").remove();
+			track.run();
 			for (i=0; i<quiz.length;i++) {
 				$("#container").append('<h2>'+ quiz[i].question + '<h2>');
 
 				for (j = 0; j < quiz[i].options.length; j++) {
-					$("#container").append("<input type='radio' name='quiz-"+i+"' value='"+quiz[i].options[j]+"'>"+quiz[i].options[j]);
+					 
+					$("#container").append("<input type='radio' name='question-"+[i]+"' value='"+quiz[i].options[j]+"'/>"+quiz[i].options[j]);
+					
 				}
 			}
 
@@ -61,74 +66,74 @@ var timer;
 
 		done: function() {
 	
-			$.each($('input[name="quiz-0]":checked'), function() {
-				if($(this).val()==quiz[0].answer){
-					track.correct++;
-					console.log(correct);
-					
-				}
-				else if ($(this).val() === '') {
-					track.unanswered++;
-				}
-				else {
-					track.incorrect++;
-					console.log(incorrect);
-				}
-			});
-			$.each($('input[name="quiz-1]":checked'), function() {
+			$.each($('input[name="question-1]":checked'), function(){
 				if($(this).val()==quiz[1].answer){
-					track.correct++;
+					correct++;
 					console.log(correct);
 					
 				}
 				else if ($(this).val() === '') {
-					track.unanswered++;
+					unanswered++;
 				}
 				else {
-					track.incorrect++;
+					incorrect++;
 					console.log(incorrect);
 				}
 			});
-			$.each($('input[name="quiz-2]":checked'), function() {
+			$.each($('input[name="question-2]":checked'), function(){
 				if($(this).val()==quiz[2].answer){
-					track.correct++;
+					correct++;
 					console.log(correct);
 					
 				}
 				else if ($(this).val() === '') {
-					track.unanswered++;
+					unanswered++;
 				}
 				else {
-					track.incorrect++;
+					incorrect++;
 					console.log(incorrect);
 				}
 			});
-			$.each($('input[name="quiz-3]":checked'), function() {
+			$.each($('input[name="question-3]":checked'), function(){
 				if($(this).val()==quiz[3].answer){
-					track.correct++;
+					correct++;
 					console.log(correct);
 					
 				}
 				else if ($(this).val() === '') {
-					track.unanswered++;
+					unanswered++;
+				}
+				else {
+					incorrect++;
+					console.log(incorrect);
+				}
+			});
+			$.each($('input[name="question-4]":checked'), function(){
+				if($(this).val()==quiz[4].answer){
+					correct++;
+					console.log(correct);
+					
+				}
+				else if ($(this).val() === '') {
+					unanswered++;
 				}
 				else {
 					track.incorrect++;
 					console.log(incorrect);
 				}
 			});
-			$.each($('input[name="quiz-4]":checked'), function() {
-				console.log("hi");
-				if($(this).val()==quiz[4].answer){
-					track.correct++;
+			$.each($('input[name="question-5]":checked'), function(){
+				if($(this).val()==quiz[5].answer){
+					correct++;
+					console.log(correct);
 					
 				}
 				else if ($(this).val() === '') {
-					track.unanswered++;
+					unanswered++;
 				}
 				else {
-					track.incorrect++;
-					console.log(track.incorrect);
+					incorrect++;
+					console.log(incorrect);
 				}
 			});
 
@@ -136,13 +141,13 @@ var timer;
 		},
 
 		result: function() {
-			clearInterval(timer);
-			$("#result h2").remove();
+			clearInterval(intervalId);
+			$("#container h2").remove();
 
 			$("#container").html("<h2>ALL DONE!<h2>");
-			$("#container").append("<h3>Correct Answers:"+ this.correct + "</h3>");
-			$("#container").append("<h3>Incorrect Answers:"+ this.incorrect + "</h3>");
-			$("#container").append("<h3>Unanswered: "+ this.unanswered +"</h3>");
+			$("#container").append("<h3>Correct Answers:"+ correct + "</h3>");
+			$("#container").append("<h3>Incorrect Answers:"+ incorrect + "</h3>");
+			$("#container").append("<h3>Unanswered: "+ unanswered +"</h3>");
 		}		
 	}
 	
@@ -150,6 +155,7 @@ var timer;
 	
 	$("#start").on("click", function() {
 		track.startTrivia();
+		
 			
 	});
 
